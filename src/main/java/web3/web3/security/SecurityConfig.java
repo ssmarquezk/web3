2f4c4d94bec8d59fb46b4c58e9d85f91f84ef66c
Sebastián Márquez Kuzina <ssmarquezk@gmail.com>
2025-12-18 19:51:34 -0300

    apertura api

--- src/main/java/web3/web3/security/SecurityConfig.java
+++ src/main/java/web3/web3/security/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/login", "/css/**", "/js/**", "/images/**", "/webjars/**", "/registro").permitAll()
+                .requestMatchers("/login", "/css/**", "/js/**", "/images/**", "/webjars/**", "/registro","/api/tutoriales","/api/usuarios").permitAll()
                 .requestMatchers("/h2-console/**").hasRole("ADMIN")
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .permitAll()
             )
             // Deshabilitar CSRF para la consola H2 (útil en entornos de desarrollo)
-            .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**","/api/tutoriales","/api/usuarios"))
             // Deshabilitar protección X-Frame-Options para permitir frames en consola H2
             .headers(headers -> headers
                 .frameOptions(frameOptions -> frameOptions.disable())
